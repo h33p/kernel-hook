f972016384531dae6447d7b198f92b7267f5ee95
Aurimas Blažulionis <0x60@pm.me>
2020-05-22 00:04:53 +0100

    Improve the tsc example

--- main.c
+++ main.c
@@ -14,18 +14,18 @@ module_exit(vmhook_fini);
 
 #define printkvm(format, ...) printk(KBUILD_MODNAME": "format, ##__VA_ARGS__)
 
-DEFINE_PER_CPU(int, hook_called_cpuid);
-
 //We do not really support multiple KVM instances here
 struct vcpu_offset_info {
 	struct kvm_vcpu *vcpu;
 	int cpu_id;
+	int called_cpuid;
 	s64 temp_offset;
 	u64 vmexit_tsc;
 } cpu_offsets[KVM_MAX_VCPUS] = {
 	{
 		.vcpu = NULL,
 		.cpu_id = -1,
+		.called_cpuid = 0,
 		.temp_offset = 0,
 		.vmexit_tsc = 0
 	}
@@ -38,6 +38,7 @@ static struct vcpu_offset_info* get_cpu_offset_info(struct kvm_vcpu *vcpu) {
 		*ret = (struct vcpu_offset_info) {
 			.vcpu = vcpu,
 			.cpu_id = -1,
+			.called_cpuid = 0,
 			.temp_offset = 0,
 			.vmexit_tsc = rdtsc()
 		};
@@ -63,6 +64,7 @@ static void vcpu_post_run(struct kvm_vcpu *vcpu) {
 
 static void vcpu_pre_run(struct kvm_vcpu *vcpu) {
 	u64 cur_tsc, off, tsc_offset, new_tsc_offset;
+	s64 tsc_shift_back;
 	struct vcpu_offset_info *off_info;
 
 	preempt_disable();
@@ -72,20 +74,24 @@ static void vcpu_pre_run(struct kvm_vcpu *vcpu) {
 	off_info = get_cpu_offset_info(vcpu);
 
 	if (off_info->cpu_id == smp_processor_id()) {
-		if (*this_cpu_ptr(&hook_called_cpuid)) {
-			*this_cpu_ptr(&hook_called_cpuid) = 0;
+		if (off_info->called_cpuid) {
 			cur_tsc = rdtsc();
 			off = -kvm_scale_tsc(vcpu, 1000 + cur_tsc - off_info->vmexit_tsc);
 			new_tsc_offset += off;
 			off_info->temp_offset += off;
 		} else { /* Shift the tsc back if not cpuid exit */
-			new_tsc_offset -= off_info->temp_offset;
-			off_info->temp_offset = 0;
+			tsc_shift_back = off_info->temp_offset;
+			if (tsc_shift_back < -500)
+				tsc_shift_back = -500;
+			new_tsc_offset -= tsc_shift_back;
+			off_info->temp_offset -= tsc_shift_back;
 		}
 
 		if (tsc_offset ^ new_tsc_offset)
 			vcpu->arch.tsc_offset = kvm_x86_ops->write_l1_tsc_offset(vcpu, new_tsc_offset);
 	}
+
+	off_info->called_cpuid = 0;
 
 	preempt_enable();
 }
@@ -94,9 +100,7 @@ DEFINE_STATIC_HOOK(int, kvm_emulate_cpuid, struct kvm_vcpu *vcpu)
 {
 	DEFINE_ORIGINAL(kvm_emulate_cpuid);
 
-	preempt_disable();
-	*this_cpu_ptr(&hook_called_cpuid) = 1;
-	preempt_enable();
+	get_cpu_offset_info(vcpu)->called_cpuid = 1;
 
 	return orig_fn(vcpu);
 }
